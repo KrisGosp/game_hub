6696b822233a5fb7ea9c93b67f505de477ea3b59
Kris Gospodinov <kris060601@gmail.com>
2024-01-23 12:01:52 +0200

    Handling game with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import NoImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return NoImage;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
