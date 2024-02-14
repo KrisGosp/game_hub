15827fd3f3fdabec6377a3e118b86401c764f259
Kris Gospodinov <kris060601@gmail.com>
2024-02-14 15:37:42 +0200

    Moving entity interfaces

--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -1,23 +1,10 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient, { FetchRes } from "../services/api-client";
-import { Platform } from "./usePlatforms";
 import ms from "ms";
 import useGameQueryStore from "../store";
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
-export type Game = {
-  id: number;
-  name: string;
-  slug: string;
-  description_raw: string;
-  background_image: string;
-  parent_platforms: {
-    platform: Platform;
-  }[];
-  metacritic: number;
-  rating_top: number;
-};
-
 const useGames = () => {
   const gameQuery = useGameQueryStore((s) => s.gameQuery);
   return useInfiniteQuery<FetchRes<Game>, Error>({
